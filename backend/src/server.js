78445f8a0f0f6d53eca7c57068d3346fd675266d
ruxiuhsiao <40812662+ruxiuhsiao@users.noreply.github.com>
2023-01-02 20:53:14 +0800

    no

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,8 +24,8 @@ if (process.env.NODE_ENV === "production") {
      const __dirname = path.resolve();
      app.use(express.static(path.join(__dirname, "../frontend", "build")));
      app.get("/*", function (req, res) {
-       res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
-     });
+          res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+        });
 }
 
 
